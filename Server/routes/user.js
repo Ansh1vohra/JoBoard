e247eebdcb8ef4920c748b4677431fba9d1727e2
Ansh1vohra <anshvohra1@gmail.com>
2024-07-14 21:44:05 +0530

    addingOutlookMail

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -7,10 +7,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'Gmail', // or any other email service
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
-        user: process.env.EMAIL_USER, // replace with your email
-        pass: process.env.EMAIL_PASS   // replace with your email password
+        user: process.env.EMAIL_USER, // replace with your Outlook email
+        pass: process.env.EMAIL_PASS  // replace with your app password
+    },
+    tls: {
+        ciphers: 'SSLv3'
     }
 });
 
