c210e1f3207c83fbdecb41e4e29a8eca192e9bc3
Ansh1vohra <anshvohra1@gmail.com>
2025-01-31 01:53:29 +0530

    addingAboutInformation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,12 @@
 import "./Home.css";
 
-export default function About(){
-    return(
+export default function About() {
+    return (
         <main className="aboutContent">
             <h2>About Us</h2>
             <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, expedita vitae fuga eius sint porro. Repellat maiores culpa saepe id asperiores quos, provident ut nam possimus doloremque architecto, nisi corporis?
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, expedita vitae fuga eius sint porro. Repellat maiores culpa saepe id asperiores quos, provident ut nam possimus doloremque architecto, nisi corporis?
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, expedita vitae fuga eius sint porro. Repellat maiores culpa saepe id asperiores quos, provident ut nam possimus doloremque architecto, nisi corporis?
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, expedita vitae fuga eius sint porro. Repellat maiores culpa saepe id asperiores quos, provident ut nam possimus doloremque architecto, nisi corporis?
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, expedita vitae fuga eius sint porro. Repellat maiores culpa saepe id asperiores quos, provident ut nam possimus doloremque architecto, nisi corporis?
+                JoBoard is a user-friendly job board web application built with the MERN stack (MongoDB, Express.js, React, Node.js), designed to connect job seekers with potential employers in an efficient and seamless way. Whether you're looking for your next career opportunity or searching for the perfect candidate to fill a position, JoBoard provides a comprehensive platform for both job postings and searches.
+                Our mission is to streamline the job search and hiring process by creating a simple, intuitive, and effective platform that benefits both job seekers and employers alike. With a focus on ease of use, fast performance, and robust functionality, JoBoard is dedicated to helping you find the right match—quickly and effortlessly.
             </p>
         </main>
     )
