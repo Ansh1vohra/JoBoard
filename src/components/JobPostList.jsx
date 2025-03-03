aa1d9caad2066b7a8b2db502df485ad744b44e74
Ansh1vohra <anshvohra1@gmail.com>
2025-03-04 00:24:23 +0530

    updatingJobPostList

--- src/components/JobPostList.jsx
+++ src/components/JobPostList.jsx
@@ -16,7 +16,7 @@ export default function JobPostList({ companyDetails }) {
         const fetchJobsByCompanyId = async (companyId) => {
             setLoading(true);
             try {
-                const response = await fetch('https://jo-board-server.vercel.app/api/jobs/getJobByCID', {
+                const response = await fetch('https://jo-board.vercel.app/api/jobs/getJobByCID', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
